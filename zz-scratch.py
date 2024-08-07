ee18ab80900ca926e77654b203a37a7b2cd86b32
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2024-08-07 16:38:39 -0400

    Tweak python scripts

--- zz-scratch.py
+++ zz-scratch.py
@@ -1,6 +1,10 @@
+import xarray as xr
+
+floodfile = "/discover/nobackup/smahanam/forMukul/SSP245_CONUS_flood_202501.nc4"
+
+dat = xr.open_dataset(floodfile, engine="h5netcdf")
+
 best_fname = "/discover/nobackup/smahanam/forMukul/Best5_CMIP6_model.nc4"
-
-
 
 
 # Integer code
